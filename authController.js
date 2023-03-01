558f46b47400c2330c802b07924ae867ea755700
turnPenny <bohdan.miroshnik23@gmail.com>
2023-03-01 15:36:26 +0100

    login function

--- authController.js
+++ authController.js
@@ -1,17 +1,32 @@
 const User = require('./models/User');
 const Role = require('./models/Role');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
+const { secret } = require('./config');
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+  return jwt.sign(payload, secret, { expiresIn: '24h' });
+};
 
 class authController {
   async register(req, res) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'registration error' });
+      }
       const { username, password } = req.body;
       const candidate = User.findOne({ username });
-      if (candidate) {
-        return res
-          .status(400)
-          .json({ message: 'User with this name already exists' });
-      }
+      // if (candidate) {
+      //   return res
+      //     .status(400)
+      //     .json({ message: `'User with this name already exists'` });
+      // }
       const hashedPassword = bcrypt.hashSync(password, 7);
       const userRole = await Role.findOne({ value: 'user' });
       const user = new User({
@@ -28,6 +43,18 @@ class authController {
   }
   async login(req, res) {
     try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(400).json({ message: `user ${user} not found` });
+      }
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: 'password is wrong' });
+      }
+
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({ token });
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: 'login error' });
--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,15 @@
 const Router = require('express');
 const router = new Router();
 const { register, login, getUsers } = require('./authController');
+const { check } = require('express-validator');
 
-router.post('/register', register);
+router.post('/register', register, [
+  check('username', 'username cannot be empty').notEmpty(),
+  check(
+    'password',
+    'password should be at least 4 signs and maximum 10'
+  ).isLength({ min: 4, max: 10 }),
+]);
 router.post('/login', login);
 router.get('/users', getUsers);
 
